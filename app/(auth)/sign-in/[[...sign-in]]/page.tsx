978ceefbde1a2b8df6b8b4c5b52e14d2e17b17b8
Brandon Thomas <lemur767trap@gmail.com>
2024-03-18 17:32:38 -0400

    database troube

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import Robot from '@/components/Spline'
 import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => {
-  return <SignIn />
+  return (
+        <>        
+        <div className="sign-up-main">
+        <SignIn />
+        </div>
+        <div className="threed-asset">
+          <Robot/>
+        </div>
+        </>
+  )
 }
 
 export default SignInPage
